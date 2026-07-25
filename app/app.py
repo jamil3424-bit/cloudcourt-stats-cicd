183bea3b20d93f507e22ec05c173f61e1f759043
jamil <jamil3424@gmail.com>
2026-07-25 00:38:42 -0400

    Update Splash Wing ppg stat

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 players = [
     {"id": 1, "name": "Point God", "ppg": 18.4, "apg": 11.2},
-    {"id": 2, "name": "Splash Wing", "ppg": 27.9, "apg": 4.1},
+    {"id": 2, "name": "Splash Wing", "ppg": 28.1, "apg": 4.1},
     {"id": 3, "name": "Rim Protector", "ppg": 12.3, "bpg": 3.5},
 ]
 
